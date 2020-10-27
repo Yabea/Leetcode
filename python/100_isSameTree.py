51bcb2a3b7887a3256bb8ce3a655bd0088603e59
Yabea <847511885@qq.com>
2020-10-27 23:30:48 +0800

    相同的树

---
+++ python/100_isSameTree.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+"""
+给定两个二叉树，编写一个函数来检验它们是否相同。
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+"""
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        elif p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
