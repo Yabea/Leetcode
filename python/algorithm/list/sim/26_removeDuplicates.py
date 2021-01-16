75e99e56cf9afc1ec71a0558b185bce1eef8560d
ben <847511885@qq.com>
2021-01-16 17:31:35 +0800

    删除排序数组中的重复项

---
+++ python/algorithm/list/sim/26_removeDuplicates.py
@@ -0,0 +1,69 @@
+# coding:utf-8
+"""
+26. 删除排序数组中的重复项
+给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+ 
+
+示例 1:
+
+给定数组 nums = [1,1,2],
+
+函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+
+你不需要考虑数组中超出新长度后面的元素。
+示例 2:
+
+给定 nums = [0,0,1,1,1,2,2,3,3,4],
+
+函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+
+你不需要考虑数组中超出新长度后面的元素。
+
+解题难点：
+nums是引用
+
+"""
+
+#
+# class Solution1(object):
+#     def removeDuplicates(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         index = len(nums) - 2
+#         while index >= 0:
+#             if nums[index] in nums[index+1:]:
+#                 nums.pop(index)
+#             index -= 1
+#         return len(nums)
+
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums[:] = sorted(list(set(nums)))
+        return len(nums)
+
+
+class Solution3(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        len_nums = len(nums)
+        index = 0
+
+        for i in range(len_nums):
+            if nums[i] != nums[index]:
+                index += 1
+                nums[index] = nums[i]
+
+        return index + 1
