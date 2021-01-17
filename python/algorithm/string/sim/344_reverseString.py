2a06653ce7079ab5461222d9da89c879ceb576f1
ben <847511885@qq.com>
2021-01-17 17:14:49 +0800

    反转字符串

---
+++ python/algorithm/string/sim/344_reverseString.py
@@ -0,0 +1,20 @@
+# coding:utf-8
+
+"""
+编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+
+不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+"""
+
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        len_s = len(s)
+
+        for i in range(len_s // 2):
+            s[i], s[-(i+1)] = s[-(i+1)], s[i]
+        return s
